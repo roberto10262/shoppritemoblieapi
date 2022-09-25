3a07c48d0267b166df6653beed5810f2960f6446
Roberto Raúl <roberto10262@gmail.com>
2022-09-26 01:00:57 +0200

    prima error codes added

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const PORT = process.env.PORT? process.env.PORT : 5000
-console.log(process.env.DATABASE_URL)
+
 
 app.listen(PORT, ()=>console.log(`running on port ${PORT}`))
